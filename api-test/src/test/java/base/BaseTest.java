6bb0b970daf07ec0bc5a4260cf5cd697faa68413
Atakan Camurcu <atkncamurcu@hotmail.com>
2025-08-03 02:19:53 +0300

    Add BaseTest class for API test configuration with RestAssured

---
+++ api-test/src/test/java/base/BaseTest.java
@@ -0,0 +1,77 @@
+package base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+/**
+ * Base test class for API tests
+ * Contains common configuration and setup for RestAssured
+ */
+public class BaseTest {
+    
+    protected static final Logger logger = LoggerFactory.getLogger(BaseTest.class);
+    protected static final String BASE_URI = "https://petstore.swagger.io/v2";
+    
+    protected RequestSpecification requestSpec;
+    protected ResponseSpecification responseSpec;
+    
+    @BeforeClass
+    public void setUp() {
+        logger.info("Setting up API test configuration");
+        
+        // Configure RestAssured base URI
+        RestAssured.baseURI = BASE_URI;
+        
+        // Create request specification
+        requestSpec = new RequestSpecBuilder()
+                .setContentType("application/json")
+                .setAccept("application/json")
+                .addFilter(new AllureRestAssured())
+                .log(LogDetail.ALL)
+                .build();
+        
+        // Create response specification (simple logging only)
+        responseSpec = new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .build();
+        
+        // Set global specifications
+        RestAssured.requestSpecification = requestSpec;
+        // Don't set global response spec to avoid status code failures
+        
+        logger.info("API test configuration completed. Base URI: {}", BASE_URI);
+    }
+    
+    /**
+     * Get the base URI for tests
+     * @return base URI string
+     */
+    protected String getBaseUri() {
+        return BASE_URI;
+    }
+    
+    /**
+     * Log test start
+     * @param testName name of the test
+     */
+    protected void logTestStart(String testName) {
+        logger.info("========== Starting Test: {} ==========", testName);
+    }
+    
+    /**
+     * Log test end
+     * @param testName name of the test
+     */
+    protected void logTestEnd(String testName) {
+        logger.info("========== Completed Test: {} ==========", testName);
+    }
+}
